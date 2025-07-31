b1ba4625a9b3a579624e09c611c106bc31103b68
FatihTheDev <fatihtheg123@gmail.com>
2025-07-31 15:51:49 +0200

    Returned treesitter.lua to its minimal state - debug purposes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,69 +1,15 @@
--- Customize Treesitter (This is for syntax highlighting)
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      -- "clojure",
-      -- "cmake",
-      -- "csv",
-      -- "dart",
-      -- "vue",
-      -- "vim"
-      -- "dockerfile",
-      -- "elixir",
-      -- "erlang",
-      -- "fsharp",
-      -- "go",
-      -- "graphql",
-      -- "haskell",
-      -- "kotlin",
-      -- "htmldjango",
-      -- "jsdoc",
-      -- "javadoc",
-      -- "julia",
-      -- "latex",
-      -- "matlab",
-      -- "nginx",
-      -- "objc",   -- Objective C
-      -- "pascal",
-      -- "perl",
-      -- "prolog",
-      -- "qmljs",
-      -- "regex",
-      -- "ruby",
-      -- "rust",
-      -- "solidity",
-      -- "svelte",
-      -- "swift",
-      --
       "lua",
-      "c",
-      "c_sharp",
-      "cpp",
-      "css",
-      "html",
-      "java",
-      "javascript",
-      "json",
-      "markdown",
-      "markdown_inline",
-      "php",
-      "python",
-      "scss",
-      "sql",
-      "tsx",
-      "typescript"
+      "vim",
       -- add more arguments for adding more treesitter parsers
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true;
     },
   },
 }
